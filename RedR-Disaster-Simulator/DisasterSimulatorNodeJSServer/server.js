18ef422261140e4bf3876ebc5bed93b17636b31a
Lachlan Keene <keenelach@ecs.vuw.ac.nz>
2019-05-13 20:46:06 +1200

    Further Proof of concept implemented messaging indirectly from mysql using socket io
    Former-commit-id: 10a5b0d5fa0df02e1162a9c0507a0cb5ecdc1695

---
+++ RedR-Disaster-Simulator/DisasterSimulatorNodeJSServer/server.js
@@ -0,0 +1,84 @@
+var path = require('path');
+var ip = require('ip');
+var opn = require('opn');
+var fs = require('fs');
+var mysql = require('mysql');
+var express = require('express');
+var app = require('express')();
+var http = require('http').createServer(app);
+var io = require('socket.io')(http);
+var message;
+
+app.use(express.static('resources'));
+
+app.get('/', function(req, res){  
+  console.log('request: ' + req.url);    
+  res.sendFile(__dirname + '/index.html');
+});
+
+
+app.get('/trainee', function(req, res){  
+  console.log('request: ' + req.url);    
+  res.sendFile(__dirname + '/trainee.html');
+});
+
+app.get('/overseer', function(req, res){  
+  console.log('request: ' + req.url);    
+  res.sendFile(__dirname + '/overseer.html');
+});
+
+io.on('connection', function(socket){
+  socket.on('message', function(msg){
+    //io.emit('message', msg);
+    var latestMessage = msg;
+    console.log(msg);
+    saveMessage(latestMessage);
+
+    con.query("SELECT message FROM communication", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+    var res = (result[0].message);
+    io.emit('message', res);
+    });
+
+
+  });
+});
+    
+
+http.listen(80, function(){
+  console.log('running');
+});
+
+
+var ipAdd = ip.address();
+opn('http://'+ipAdd);
+
+
+
+//console.dir (ip.address());
+
+
+
+var con = mysql.createConnection({
+  host: "localhost",
+  user: "root",
+  password: "root",
+  database: "mydb"
+ 
+});
+
+con.connect(function(err) {
+  if (err) throw err;
+  console.log("Connected!");
+  
+});
+
+function saveMessage(latestMessage){
+  console.log(latestMessage);
+  var sql = "INSERT INTO communication (message) VALUES ('" + latestMessage + "') ";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("message saved");
+  });
+}
