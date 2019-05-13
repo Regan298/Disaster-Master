14ff3dd27b4e8d0241d67e55bcedf6d310e22c7b
Lachlan Keene <keenelach@ecs.vuw.ac.nz>
2019-05-13 13:09:19 +1200

    Proof of concept code base basically includes portable nodejs server, ommited the mysql files as they are to large to store, however implementation is functional
    Former-commit-id: d51bc6174c2193a2ac66a0d3355e436986463608

--- RedR Disaster Simulator/DisasterSimulatorNodeJSServer/server.js
+++ RedR Disaster Simulator/DisasterSimulatorNodeJSServer/server.js
@@ -41,10 +41,31 @@ if(req.url === '/overseer'){
 var con = mysql.createConnection({
   host: "localhost",
   user: "root",
-  password: "root"
+  password: "root",
+  database: "mydb"
+ 
 });
 
 con.connect(function(err) {
   if (err) throw err;
   console.log("Connected!");
+  
+
+  var sql = "CREATE TABLE customers (name VARCHAR(255), address VARCHAR(255))";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("Table created");
+  });
+
+  var sql = "INSERT INTO customers (name, address) VALUES ('Company Inc', 'Highway 37')";
+  con.query(sql, function (err, result) {
+    if (err) throw err;
+    console.log("1 record inserted");
+  });
+
+  con.query("SELECT * FROM customers", function (err, result, fields) {
+    if (err) throw err;
+    console.log(result);
+  });
+   
 });
