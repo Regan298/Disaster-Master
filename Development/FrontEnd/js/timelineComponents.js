60b8bf1f3819b251c8a2310078c0a9d63f8a0a41
Daniel Miller <millerdani1@ecs.vuw.ac.nz>
2019-06-05 13:05:28 +1200

    implemented custom scale for majorLabels (not functional)
    Former-commit-id: 8d0aada94defabce34f053bacdb8442ddbbcfe2a

--- Development/FrontEnd/js/timelineComponents.js
+++ Development/FrontEnd/js/timelineComponents.js
@@ -6,26 +6,6 @@ var groups = new vis.DataSet([
 ]);
 
 var items = new vis.DataSet([{
-  id: 1,
-  group: 1,
-  content: 'item 1',
-  start: '2019-05-10 04:30:00'
-}, {
-  id: 2,
-  group: 2,
-  content: 'item 2',
-  start: '2019-05-12 16:30:00'
-}, {
-  id: 3,
-  group: 3,
-  content: 'item 3',
-  start: '2019-05-15 09:00:00'
-}, {
-  id: 4,
-  group: 4,
-  content: 'item 4',
-  start: '2019-05-13 12:00:00'
-}, {
   id: 5,
   group: 4,
   content: 'item 5',
@@ -40,15 +20,106 @@ var options = {
   //for making timeline scrollable
   verticalScroll: true,
   maxHeight: timelineHeight,
-  
   orientation: 'bottom',
-  min: new Date('2019-05-10 00:00:00'),
-  max: new Date('2019-05-15 24:00:00'),
-  zoomMin: 1000 * 4 * 60 * 24 * 7,
+  min: new Date('2019-01-01 00:00:00'),
+  max: new Date('2019-01-05 24:00:00'),
+  zoomMin: 9000000,
   margin: {
       item: 10,
       axis: 5
+    },
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    format: {
+      //minorLabels: function(date, scale, step) {
+        //var now = new Date();
+        //var ago = now - date;
+        //var divider;
+        //switch (scale) {
+         // case 'millisecond':
+         //   divider = 1;
+         //   break;
+         // case 'second':
+         //   divider = 1000;
+          //  break;
+          //case 'minute':
+          //  divider = 1000 * 60;
+          //  break;
+          //case 'hour':
+          //  divider = 1000 * 60 * 60;
+          //  break;
+          //case 'day':
+          //  divider = 1000 * 60 * 60 * 24;
+          //  break;
+          //case 'weekday':
+          //  divider = 1000 * 60 * 60 * 24 * 7;
+          //  break;
+          //case 'month':
+           // divider = 1000 * 60 * 60 * 24 * 30;
+          //  break;
+          //case 'year':
+           // divider = 1000 * 60 * 60 * 24 * 365;
+          //  break;
+         // default:
+        //    return new Date(date);
+       // } 
+       // return  (Math.round(ago * step / divider)) + " " + scale + "s ago" 
+     // },
+        majorLabels: function(date, scale, step) {
+            var label="";
+          
+            switch (scale) {
+            case 'millisecond':
+
+                break;
+            case 'second':
+
+                break;
+            case 'minute':
+                label="Day x";
+                break;
+            case 'hour':
+                label="Day x";
+                break;
+            case 'day':
+                
+                break;
+            case 'weekday':
+                
+                break;
+            case 'month':
+
+                break;
+            case 'year':
+
+                break;
+            default:
+                return ("ERROR: majorLabels");
+            }
+            return (label);
+        }
     }
+    
+    //  majorLabels: {
+    //millisecond:'HH:mm:ss',
+    //second:     'D MMMM HH:mm',
+    //minute:     'ddd D MMMM',
+    //hour:       'ddd D MMMM',
+    //weekday:    'MMMM YYYY',
+    //day:        'MMMM YYYY',
+    //week:       'MMMM YYYY',
+    //month:      'YYYY',
+    //year:       ''
+  //}
+//}
+    
 };
 
 //redraw()
