a2e84cc9eb5ba9a8a68bdf4f0a05e889e88e6e41
Daniel Miller <millerdani1@ecs.vuw.ac.nz>
2019-07-16 10:49:40 +1200

    timeline zoomMin and zoomMax set, additional formats added for minorLabel
    Former-commit-id: 63439b6ecd22dfe0906899b0dedc89f3e8747fa9

--- Development/FrontEnd/js/timelineComponents.js
+++ Development/FrontEnd/js/timelineComponents.js
@@ -6,7 +6,9 @@ var startDate = new Date('2019-01-01 00:00:00');
 //endDate.setHours('24');
 //endDate.setMinutes();
 //endDate.setSeconds();
-var endDate = new Date('2019-01-14 24:00:00');
+//var endDate = new Date();
+//endDate.setDate(startDate.getDate);
+//alert(endDate.getDate);
 
 var groups = new vis.DataSet([
     {id: 1, content: 'Group 1'},
@@ -22,22 +24,22 @@ var items = new vis.DataSet([{
   start: '2019-05-10 14:00:00'
 }]);
 
-var timelineHeight = window.innerHeight/1.5;
 // Config for Timeline
 var options = {
+
   editable: false,
-  
-  //for making timeline scrollable
+  showCurrentTime: false,
   verticalScroll: true,
-  maxHeight: timelineHeight,
+  maxHeight: window.innerHeight/1.5,
   orientation: 'bottom',
   
   
   min: startDate,
-  max: endDate,
+  //max: endDate,
   
   
-  //zoomMin: 9000000,
+  zoomMin: 9000000,
+  zoomMax: 3000000000,
   margin: {
       item: 10,
       axis: 5
@@ -51,10 +53,8 @@ var options = {
             var minorLabel='';
             switch (scale) {
             case 'millisecond':
-
                 break;
             case 'second':
-
                 break;
             case 'minute':
                     var tempDate = new Date(date);
@@ -65,7 +65,9 @@ var options = {
                 minorLabel = ("0" + tempDate.getHours()).slice(-2) + ":" + ("0" + tempDate.getMinutes()).slice(-2);
                 break;
             case 'day':
-
+                var res = Math.abs(startDate - date) / 1000;
+                var days = Math.floor(res / 86400)+1;
+                minorLabel="Day "+days;
                 break;
             case 'weekday':
                 var res = Math.abs(startDate - date) / 1000;
@@ -73,10 +75,8 @@ var options = {
                 minorLabel="Day "+days;
                 break;
             case 'month':
-
                 break;
             case 'year':
-
                 break;
             default:
                 return ("ERROR: minorLabels");
@@ -86,75 +86,33 @@ var options = {
 
         majorLabels: function(date, scale, step) {
             var majorLabel='';
-          
             switch (scale) {
             case 'millisecond':
-
                 break;
             case 'second':
-
                 break;
             case 'minute':
-
                 var res = Math.abs(startDate - date) / 1000;
                 var days = Math.floor(res / 86400)+1;
                 majorLabel="Day "+days;
-
                 break;
             case 'hour':
-                
                 var res = Math.abs(startDate - date) / 1000;
                 var days = Math.floor(res / 86400)+1;
                 majorLabel="Day "+days;
-
                 break;
             case 'day':
-                
                 break;
             case 'weekday':
-                
                 break;
             case 'month':
-
                 break;
             case 'year':
-
                 break;
             default:
                 return ("ERROR: majorLabels");
             }
             return (majorLabel);
         }
-    }
-    
-
-
-
-
-
-    // {
-    //     minorLabels: {
-    //       millisecond:'SSS',
-    //       second:     's',
-    //       minute:     'HH:mm',
-    //       hour:       'HH:mm',
-    //       weekday:    'ddd D',
-    //       day:        'D',
-    //       week:       'w',
-    //       month:      'MMM',
-    //       year:       'YYYY'
-    //     },
-    //     majorLabels: {
-    //       millisecond:'HH:mm:ss',
-    //       second:     'D MMMM HH:mm',
-    //       minute:     'ddd D MMMM',
-    //       hour:       'ddd D MMMM',
-    //       weekday:    'MMMM YYYY',
-    //       day:        'MMMM YYYY',
-    //       week:       'MMMM YYYY',
-    //       month:      'YYYY',
-    //       year:       ''
-    //     }
-    //   }
-    
+    }   
 };
