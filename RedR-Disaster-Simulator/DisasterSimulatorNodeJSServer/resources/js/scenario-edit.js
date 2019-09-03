477793679e0bb97a0a87ab39267fbfe6a2040252
Tom Buurmans <tom.buurmans@gmail.com>
2019-09-04 10:28:33 +1200

    Editor now displays in hours and minutes for duration
    Former-commit-id: f69b4e5da80d07c30c27d8e6004ad47126439348

--- RedR-Disaster-Simulator/DisasterSimulatorNodeJSServer/resources/js/scenario-edit.js
+++ RedR-Disaster-Simulator/DisasterSimulatorNodeJSServer/resources/js/scenario-edit.js
@@ -110,8 +110,42 @@ function updateTitle(){
 
 function editDuration(){
     $('#editDuration').empty();
+    var minutes = Math.floor((data.durationMs / 1000 / 60) % 60);
+    var hours = Math.floor((data.durationMs / (1000 * 60 * 60)) % 24);
     $('#editDuration').append("<form id='durationForm'></form>" +
-                            "Duration: <input form='durationForm' type='text' name='duration' value='"+data.durationMs+"'><br>"+
+                            "Hours: <select form='durationForm' name='hours' value='"+hours+"'>" +
+                            "<option value=0>0</option>" +
+                            "<option value=1>1</option>" +
+                            "<option value=2>2</option>" +
+                            "<option value=3>3</option>" +
+                            "<option value=4>4</option>" +
+                            "<option value=5>5</option>" +
+                            "<option value=6>6</option>" +
+                            "<option value=7>7</option>" +
+                            "<option value=8>8</option>" +
+                            "<option value=9>9</option>" +
+                            "<option value=10>10</option>" +
+                            "<option value=11>11</option>" +
+                            "<option value=12>12</option>" +
+                            "<option value=13>13</option>" +
+                            "<option value=14>14</option>" +
+                            "<option value=15>15</option>" +
+                            "<option value=16>16</option>" +
+                            "<option value=17>17</option>" +
+                            "<option value=18>18</option>" +
+                            "<option value=19>19</option>" +
+                            "<option value=20>20</option>" +
+                            "<option value=21>21</option>" +
+                            "<option value=22>22</option>" +
+                            "<option value=23>23</option>" +
+                            "<option value=24>24</option>" +
+                            "</select>" +
+                            "Minutes: <select form='durationForm' name='mins' value='"+minutes+"'>" +
+                            "<option value=0>0</option>" +
+                            "<option value=15>15</option>" +
+                            "<option value=30>30</option>" +
+                            "<option value=45>45</option>" +
+                            "</select><br>" +
                             "<input form='durationForm' type='button' onclick='updateDuration()' value='Submit'>" +
                             "<button type='button' onclick=cancelEdit('#editDuration')>Cancel</button>");
 }
@@ -119,7 +153,12 @@ function editDuration(){
 function updateDuration(){
     let frmData = document.getElementById("durationForm");
 
-    data.durationMs = frmData.elements[0].value;
+    var h = frmData.elements[0].value;
+    var m = frmData.elements[1].value;
+
+    var ms = (h * 60 * 60 * 1000) + (m * 60 * 1000);
+
+    data.durationMs = ms;
 
     $('#editDuration').empty();
     drawDetails(data);
